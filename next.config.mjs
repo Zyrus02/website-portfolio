d256a2d119eb6e17da3462d6d72610b3f36ffbd0
Farhan Nur Iman <farhannuriman123@gmail.com>
2026-07-15 09:32:57 +0800

    Allow local network dev origins in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    allowedDevOrigins: ["172.20.10.2", "localhost:3000"],
+  },
 };
 
 export default nextConfig;
